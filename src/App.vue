8e4676e7e3ec12db33b7c29d85c9bb69ac29449e
Guilherme Freitas <hfdsguilherme@gmail.com>
2021-11-05 10:07:18 -0300

    chore: routing

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <page-home/>
+  <div class="container">
+    <page-home/>
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import PageHome from '@/components/PageHome'
 export default {
   name: 'App',
-  components: {
-    PageHome
-  }
 }
 </script>
 
