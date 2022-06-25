2cf5b9fe05e3b529509c7aef444bb60abb85dd5a
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-06-25 11:06:40 -0300

    refactor: extract readable date to a compnent

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -23,8 +23,8 @@
           </div>
         </div>
 
-        <div class="post-date text-faded" :title="humanFriendlyDate(post.publishedAt)" >
-          {{ diffForHumans(post.publishedAt)}}
+        <div class="post-date text-faded" >
+          <AppDate :timestamp="post.publishedAt" />
         </div>
 
       </div>
@@ -34,12 +34,7 @@
 
 <script>
 import sourceData from '@/data.json'
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
-import localizedDate from 'dayjs/plugin/localizedFormat'
-
-dayjs.extend(relativeTime)
-dayjs.extend(localizedDate)
+import AppDate from '@/components/AppDate'
 
 export default {
   props: {
@@ -48,7 +43,10 @@ export default {
       type: Array
     }
   },
-    data () {
+  components: {
+    AppDate
+  },
+  data () {
     return {
       users: sourceData.users
     }
@@ -56,12 +54,6 @@ export default {
   methods: {
     userById (userId) {
       return this.users.find(p => p.id === userId)
-    },
-    diffForHumans (timeStamp) {
-      return dayjs.unix(timeStamp).fromNow()
-    },
-    humanFriendlyDate (timeStamp) {
-      return dayjs.unix(timeStamp).format('llll')
     }
   }
 }
