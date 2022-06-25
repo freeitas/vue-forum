a5fa75a6300b7d55761f68bd30f5d4650d0e2e67
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-06-25 10:37:09 -0300

    feat: human readable dates

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -23,8 +23,8 @@
           </div>
         </div>
 
-        <div class="post-date text-faded">
-          {{post.publishedAt}}
+        <div class="post-date text-faded" :title="humanFriendlyDate(post.publishedAt)" >
+          {{ diffForHumans(post.publishedAt)}}
         </div>
 
       </div>
@@ -34,6 +34,12 @@
 
 <script>
 import sourceData from '@/data.json'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import localizedDate from 'dayjs/plugin/localizedFormat'
+
+dayjs.extend(relativeTime)
+dayjs.extend(localizedDate)
 
 export default {
   props: {
@@ -50,6 +56,12 @@ export default {
   methods: {
     userById (userId) {
       return this.users.find(p => p.id === userId)
+    },
+    diffForHumans (timeStamp) {
+      return dayjs.unix(timeStamp).fromNow()
+    },
+    humanFriendlyDate (timeStamp) {
+      return dayjs.unix(timeStamp).format('llll')
     }
   }
 }
