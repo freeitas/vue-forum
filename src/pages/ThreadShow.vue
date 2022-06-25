7e1660f70f7db2573138d80835682c0802c871de
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-06-25 09:44:41 -0300

    feat: creating post editor

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -4,7 +4,16 @@
 
     <post-list :posts="threadPosts" />
 
-    
+    <div class="col-full">
+      <form @submit.prevent="addPost">
+        <div class="form-group">
+          <textarea v-model="newPostText" id="" col="30" rows="10" class="form-input" />
+        </div>
+        <div class="form-actions">
+          <button class="btn-blue">Submit post</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -26,6 +35,7 @@ export default {
     return {
       threads: sourceData.threads,
       posts: sourceData.posts,
+      newPostText: ''
     }
   },
   computed: {
@@ -34,6 +44,22 @@ export default {
     },
     threadPosts () {
       return this.posts.filter(post => post.threadId === this.id)
+    }
+  },
+  methods: {
+    addPost () {
+      const postId = 'temporary' + Math.random()
+      const post = {
+        id: postId,
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: '38St7Q8Zi2N1SPa5ahzssq9kbyp1'
+      }
+      this.posts.push(post)
+      this.thread.posts.push(postId)
+
+      this.newPostText = ''
     }
   }
 }
